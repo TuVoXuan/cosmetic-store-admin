c6bdcab5d0628400c84f76bce9ddc8468af542a5
TuVoXuan <voxuantu1235@gmail.com>
2023-02-19 11:10:17 +0700

    feat: get variation options api

--- src/views/prodcut/TabProductItem.tsx
+++ src/views/prodcut/TabProductItem.tsx
@@ -49,139 +49,12 @@ const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
   }
 }))
 
-const top100Films = [
-  { label: 'The Shawshank Redemption', year: 1994 },
-  { label: 'The Godfather', year: 1972 },
-  { label: 'The Godfather: Part II', year: 1974 },
-  { label: 'The Dark Knight', year: 2008 },
-  { label: '12 Angry Men', year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: 'Pulp Fiction', year: 1994 },
-  {
-    label: 'The Lord of the Rings: The Return of the King',
-    year: 2003
-  },
-  { label: 'The Good, the Bad and the Ugly', year: 1966 },
-  { label: 'Fight Club', year: 1999 },
-  {
-    label: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001
-  },
-  {
-    label: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980
-  },
-  { label: 'Forrest Gump', year: 1994 },
-  { label: 'Inception', year: 2010 },
-  {
-    label: 'The Lord of the Rings: The Two Towers',
-    year: 2002
-  },
-  { label: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { label: 'Goodfellas', year: 1990 },
-  { label: 'The Matrix', year: 1999 },
-  { label: 'Seven Samurai', year: 1954 },
-  {
-    label: 'Star Wars: Episode IV - A New Hope',
-    year: 1977
-  },
-  { label: 'City of God', year: 2002 },
-  { label: 'Se7en', year: 1995 },
-  { label: 'The Silence of the Lambs', year: 1991 },
-  { label: "It's a Wonderful Life", year: 1946 },
-  { label: 'Life Is Beautiful', year: 1997 },
-  { label: 'The Usual Suspects', year: 1995 },
-  { label: 'Léon: The Professional', year: 1994 },
-  { label: 'Spirited Away', year: 2001 },
-  { label: 'Saving Private Ryan', year: 1998 },
-  { label: 'Once Upon a Time in the West', year: 1968 },
-  { label: 'American History X', year: 1998 },
-  { label: 'Interstellar', year: 2014 },
-  { label: 'Casablanca', year: 1942 },
-  { label: 'City Lights', year: 1931 },
-  { label: 'Psycho', year: 1960 },
-  { label: 'The Green Mile', year: 1999 },
-  { label: 'The Intouchables', year: 2011 },
-  { label: 'Modern Times', year: 1936 },
-  { label: 'Raiders of the Lost Ark', year: 1981 },
-  { label: 'Rear Window', year: 1954 },
-  { label: 'The Pianist', year: 2002 },
-  { label: 'The Departed', year: 2006 },
-  { label: 'Terminator 2: Judgment Day', year: 1991 },
-  { label: 'Back to the Future', year: 1985 },
-  { label: 'Whiplash', year: 2014 },
-  { label: 'Gladiator', year: 2000 },
-  { label: 'Memento', year: 2000 },
-  { label: 'The Prestige', year: 2006 },
-  { label: 'The Lion King', year: 1994 },
-  { label: 'Apocalypse Now', year: 1979 },
-  { label: 'Alien', year: 1979 },
-  { label: 'Sunset Boulevard', year: 1950 },
-  {
-    label: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964
-  },
-  { label: 'The Great Dictator', year: 1940 },
-  { label: 'Cinema Paradiso', year: 1988 },
-  { label: 'The Lives of Others', year: 2006 },
-  { label: 'Grave of the Fireflies', year: 1988 },
-  { label: 'Paths of Glory', year: 1957 },
-  { label: 'Django Unchained', year: 2012 },
-  { label: 'The Shining', year: 1980 },
-  { label: 'WALL·E', year: 2008 },
-  { label: 'American Beauty', year: 1999 },
-  { label: 'The Dark Knight Rises', year: 2012 },
-  { label: 'Princess Mononoke', year: 1997 },
-  { label: 'Aliens', year: 1986 },
-  { label: 'Oldboy', year: 2003 },
-  { label: 'Once Upon a Time in America', year: 1984 },
-  { label: 'Witness for the Prosecution', year: 1957 },
-  { label: 'Das Boot', year: 1981 },
-  { label: 'Citizen Kane', year: 1941 },
-  { label: 'North by Northwest', year: 1959 },
-  { label: 'Vertigo', year: 1958 },
-  {
-    label: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983
-  },
-  { label: 'Reservoir Dogs', year: 1992 },
-  { label: 'Braveheart', year: 1995 },
-  { label: 'M', year: 1931 },
-  { label: 'Requiem for a Dream', year: 2000 },
-  { label: 'Amélie', year: 2001 },
-  { label: 'A Clockwork Orange', year: 1971 },
-  { label: 'Like Stars on Earth', year: 2007 },
-  { label: 'Taxi Driver', year: 1976 },
-  { label: 'Lawrence of Arabia', year: 1962 },
-  { label: 'Double Indemnity', year: 1944 },
-  {
-    label: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004
-  },
-  { label: 'Amadeus', year: 1984 },
-  { label: 'To Kill a Mockingbird', year: 1962 },
-  { label: 'Toy Story 3', year: 2010 },
-  { label: 'Logan', year: 2017 },
-  { label: 'Full Metal Jacket', year: 1987 },
-  { label: 'Dangal', year: 2016 },
-  { label: 'The Sting', year: 1973 },
-  { label: '2001: A Space Odyssey', year: 1968 },
-  { label: "Singin' in the Rain", year: 1952 },
-  { label: 'Toy Story', year: 1995 },
-  { label: 'Bicycle Thieves', year: 1948 },
-  { label: 'The Kid', year: 1921 },
-  { label: 'Inglourious Basterds', year: 2009 },
-  { label: 'Snatch', year: 2000 },
-  { label: '3 Idiots', year: 2009 },
-  { label: 'Monty Python and the Holy Grail', year: 1975 }
-]
-
 interface FormValue {
   product: IProductNameOption
   thumbnail: any
   images: any
   price: number
-  // [String]: string
+  [index: string]: any
 }
 
 const TabProductItem = () => {
@@ -192,9 +65,6 @@ const TabProductItem = () => {
   const [imgSrc, setImgSrc] = useState<string[]>(['/images/avatars/1.png'])
   const [productNames, setProductNames] = useState<IProductNameOption[]>([])
   const [variationsGroup, setVariationsGroup] = useState<IVariationGroup[]>([])
-
-  // ** Ref
-  const variationContainerRef = useRef<HTMLDivElement>(null)
 
   // ** react-hook-form
   const {
@@ -211,7 +81,7 @@ const TabProductItem = () => {
     }
   })
 
-  const watchProduct = watch('product.value')
+  const watchProduct = watch('product')
 
   const onChangeThumbnail = (file: ChangeEvent) => {
     const { files } = file.target as HTMLInputElement
@@ -264,13 +134,13 @@ const TabProductItem = () => {
 
   const handleGetVariationOptions = async () => {
     try {
-      console.log('vo')
       const product = getValues('product')
       const optionsGroup: IVariationGroup[] = []
-      for (const variationId of product.variations) {
+      for (let i = 0; i < product.variations.length; i++) {
+        const variationId: string = product.variations[i]
         const res = await variationApi.getVariationOptions(variationId)
         let variationName: string = ''
-        res.data.data.variationName.name.forEach(item => {
+        res.data.data.variation.name.forEach(item => {
           if (item.language === 'vi') {
             variationName = item.value
           }
@@ -278,27 +148,23 @@ const TabProductItem = () => {
 
         optionsGroup.push({
           variationName: variationName,
-          variationOptions: res.data.data.variationOptions
-        })
+          variationOptions: res.data.data.variationOptions.map(item => {
+            let label = ''
+            item.value.forEach(name => {
+              if (name.language === 'vi') {
+                label = name.value
+              }
+            })
 
-        if (variationContainerRef.current) {
-          variationContainerRef.current.innerHTML += (
-            <Grid item xs={12} sm={6}>
-              <Autocomplete
-                disablePortal
-                id={variationName}
-                multiple
-                options={top100Films}
-                sx={{ width: '100%' }}
-                renderInput={params => <TextField {...params} label={variationName} />}
-              />
-            </Grid>
-          )
-        }
+            return {
+              label,
+              value: item._id
+            }
+          })
+        })
       }
 
-      setVariationsGroup(optionsGroup)
-      console.log(variationsGroup)
+      setVariationsGroup([...optionsGroup])
     } catch (error) {
       console.log('error: ', error)
     }
@@ -306,15 +172,18 @@ const TabProductItem = () => {
 
   const onSubmit = (data: any) => {
     console.log('data: ', data)
+    variationsGroup.forEach(element => {
+      console.log('data[element.variationName]: ', data[element.variationName])
+    })
   }
 
   useEffect(() => {
     handleGetProductNames()
   }, [])
 
-  // useEffect(() => {
-  //   handleGetVariationOptions()
-  // }, [watchProduct])
+  useEffect(() => {
+    setVariationsGroup([])
+  }, [watchProduct])
 
   return (
     <Fragment>
@@ -347,7 +216,6 @@ const TabProductItem = () => {
                     variant='outlined'
                     onClick={() => {
                       setValue('thumbnail', undefined)
-                      console.log('thumbnail: ', thumbnail)
                       setThumbnail('/images/avatars/1.png')
                     }}
                   >
@@ -449,10 +317,35 @@ const TabProductItem = () => {
               />
             </Grid>
 
-            <div ref={variationContainerRef}></div>
+            {variationsGroup.map(item => (
+              <Grid item xs={12} sm={6} key={item.variationName}>
+                <Controller
+                  name={item.variationName}
+                  rules={{ required: { value: true, message: `${item.variationName} is required` } }}
+                  control={control}
+                  render={({ field: { onChange, value }, fieldState: { error, invalid } }) => (
+                    <Autocomplete
+                      disablePortal
+                      isOptionEqualToValue={(option, value) => option.value === value.value}
+                      id={item.variationName}
+                      options={item.variationOptions}
+                      sx={{ width: '100%' }}
+                      renderInput={params => (
+                        <TextField {...params} error={invalid} helperText={error?.message} label={item.variationName} />
+                      )}
+                      onChange={(e, value) => {
+                        onChange(value)
+                        handleGetVariationOptions()
+                        return value
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+            ))}
 
             <Grid item xs={12}>
-              <Button onClick={() => console.log(errors)} variant='contained' type='submit' sx={{ marginRight: 3.5 }}>
+              <Button variant='contained' type='submit' sx={{ marginRight: 3.5 }}>
                 Save
               </Button>
             </Grid>
