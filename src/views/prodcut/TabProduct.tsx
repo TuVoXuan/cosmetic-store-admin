88281042e83aebcae7660ffeaef759aa994ad273
VoMinhTuan21 <vominhtuan.8a1@gmail.com>
2023-03-10 08:14:38 +0700

    style: set height for quill editor

--- src/views/prodcut/TabProduct.tsx
+++ src/views/prodcut/TabProduct.tsx
@@ -69,12 +69,8 @@ const TabProduct = () => {
   const modules = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-      ['blockquote', 'code-block'],
       [{ header: 1 }, { header: 2 }], // custom button values
       [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-      // [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-      // [{ direction: "rtl" }], // text direction
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       [{ color: [] }, { background: [] }], // dropdown with defaults from theme
       [{ align: [] }],
@@ -91,7 +87,6 @@ const TabProduct = () => {
     'strike',
     'color',
     'background',
-    'script',
     'header',
     'blockquote',
     'list',
